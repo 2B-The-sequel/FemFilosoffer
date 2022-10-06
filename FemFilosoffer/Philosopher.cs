1c791ca1f51059400ab5a1e3f73dd62b7b6cf4cd
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2022-10-06 13:57:23 +0200

    Add project files.

---
+++ FemFilosoffer/Philosopher.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace FemFilosoffer
+{
+    internal class Philosopher
+    {
+        private readonly object leftChopstick;
+        private readonly object rightChopstick;
+
+        private readonly string name;
+        private int eatCount = 0;
+
+        public Philosopher(string name, object left, object right)
+        {
+            this.name = name;
+            leftChopstick = left;
+            rightChopstick = right;
+        }
+
+        public void Eat()
+        {
+            while (eatCount < 10)
+            {
+                Random random = new();
+                TimeSpan timeout = TimeSpan.FromMilliseconds(random.Next(0,100));
+                
+                bool leftTaken = false;
+                bool rightTaken = false;
+                
+                // TRY LOCK LEFT
+                try
+                {
+                    Monitor.TryEnter(leftChopstick, timeout, ref leftTaken);
+                    if (leftTaken)
+                    {
+                        Thread.Sleep(500);
+
+                        // TRY LOCK RIGHT
+                        try
+                        {
+                            Monitor.TryEnter(rightChopstick, timeout, ref rightTaken);
+                            if (rightTaken)
+                            {
+                                // EAT
+                                eatCount++;
+                                Thread.Sleep(10);
+                                Console.WriteLine($"{name} has eaten: {eatCount} times");
+                            }
+                        }
+                        finally
+                        {
+                            // UNLOCK RIGHT
+                            if (rightTaken)
+                            {
+                                Monitor.Exit(rightChopstick);
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    // UNLOCK LEFT
+                    if (leftTaken)
+                    {
+                        Monitor.Exit(leftChopstick);
+                    }
+                }
+
+                // SLEEP
+                Thread.Sleep(10);
+            }
+
+        }
+
+        /*public void Eat()
+        {
+            while (eatCount < 10) {
+                lock (leftChopstick)
+                {
+                    Thread.Sleep(500);
+                    lock (rightChopstick)
+                    {
+                        eatCount++;
+                        Thread.Sleep(10);
+                        Console.WriteLine($"{name} has eaten: {eatCount} times");
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }*/
+    }
+}
---
+++ FemFilosoffer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace FemFilosoffer
+{
+    public class Program
+    {
+        private static Thread[] threads;
+        private static Philosopher[] philosophers;
+        private static readonly string[] names = { "Kevin", "Peter", "Kasper", "Martin", "Allan" };
+        private static object[] chopsticks;
+
+        public static void Main(string[] args)
+        {
+            chopsticks = new object[names.Length];
+            threads = new Thread[names.Length];
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                chopsticks[i] = new object();
+            }
+
+            philosophers = new Philosopher[names.Length];
+
+            Stopwatch watch = new();
+
+            // MULTITHREADED
+            watch.Start();
+            for (int i = 0; i < names.Length; i++)
+            {
+                object leftChopstick = chopsticks[i == 0 ? names.Length-1 : i-1];
+                object rightChopstick = chopsticks[i];
+
+                philosophers[i] = new Philosopher(names[i], leftChopstick, rightChopstick);
+                threads[i] = new(philosophers[i].Eat);
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
+
+            watch.Stop();
+            long multithreadedTime = watch.ElapsedMilliseconds;
+
+            // SEQUENTIAL
+            watch.Restart();
+            for (int i = 0; i < names.Length; i++)
+            {
+                object leftChopstick = chopsticks[i == 0 ? names.Length - 1 : i - 1];
+                object rightChopstick = chopsticks[i];
+
+                philosophers[i] = new Philosopher(names[i], leftChopstick, rightChopstick);
+                philosophers[i].Eat();
+            }
+            watch.Stop();
+            long sequentialTime = watch.ElapsedMilliseconds;
+
+            // WRITE TIMES
+            Console.WriteLine("Multithreaded: " + multithreadedTime);
+            Console.WriteLine("Sequential: " + sequentialTime);
+
+            Console.ReadLine();
+        }
+    }
+}
